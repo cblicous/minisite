fe04ed802fb012172edd9f86d49b4d3339769e33
cb <cb@cb.de>
2012-02-15 11:51:04 +0100

    init

---
+++ db/migrate/20120214125107_create_entries.rb
@@ -0,0 +1,11 @@
+class CreateEntries < ActiveRecord::Migration
+  def change
+    create_table :entries do |t|
+      t.string :name
+      t.string :title
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
