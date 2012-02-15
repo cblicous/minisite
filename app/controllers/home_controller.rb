c6fa4fae369efdddc73fd3fed858535db958d0d2
cb <cb@cb.de>
2012-02-15 15:38:58 +0100

    Added Frontpage controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    @entries = Entry.all
+
+    respond_to do |format| # Calles the responder on the AppkicationController .. do ... end is the parameter 
+		#|format| stands for a single argument taken 
+		
+      format.html # index.html.erb
+      format.json { render :json => @entries }
+    end
   end
+
 end
